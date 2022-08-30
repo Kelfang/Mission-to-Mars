77a9d61a63dafad1f0ae743b13c80e22c2989ca0
Kelly Fangmann <kellyfangmann@KRF-MacBook-Pro.local>
2022-08-30 11:48:49 -0500

    updated files

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ from flask_pymongo import PyMongo
 import scraping
 
 # Set up Flask.
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Use Python to connect to Mongo using PyMongo.
-app.config["MONGO_URI"] = 'mongodb://localhost:27017/mars_app'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Define the route for the HTML page.
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+   mars = mongo.db.mars.find_one()
+   return render_template('index.html',mars=mars)
 
 # Define the route for scraping the data.
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set":mars_data}, upsert=True)
-    return redirect('/', code=302)
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
 
 # Run Flask.
 if __name__ == "__main__":
-    app.run()
+   app.run(debug=True)
--- scraping.py
+++ scraping.py
@@ -3,80 +3,119 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+
+def scrape_all():
+
+    # Set up executable path.
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    news_title, news_summary = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary.
+    data = {
+        'news_title': news_title,
+        'news_summary': news_summary,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(),
+        'last_modified': dt.datetime.now()}
+
+    # End session/call.
+    browser.quit
+    return data
 
 
-# Set up executable path.
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def mars_news(browser):
+
+    # Visit the Mars NASA news site.
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    # Set up delay for loading the page - optional.
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
 
-# Visit the Mars NASA news site.
-url = 'https://redplanetscience.com'
-browser.visit(url)
+    # Set up the HTML parser.
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
 
-# Set up delay for loading the page - optional.
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Add try/except for error handling. 
+    try:
+        # Set variables and start scraping. 
+        slide_elem = news_soup.select_one('div.list_text')
 
-
-# Set up the HTML parser.
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
-
-
-# Create scraping code.
-slide_elem.find('div', class_='content_title')
+        # Set variables and start scraping. 
+        slide_elem.find('div', class_='content_title')
 
 
-# Use the parent element to find the first 'a' tag and save it as 'news_title'.
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'.
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+    
 
+        # Find the news article summary.
+        news_summary = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-# Find the news article summary.
-news_summary = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_summary
+    except AttributeError:
+        return None, None
 
+    return news_title, news_summary
 
 # ### Featured Images# 
 
+def featured_image(browser):
 
-# Visit URL for images.
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
-
-
-# Find and click the full image button. 
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Visit URL for images.
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
 
-# Parse the resulting html with soup.
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Find and click the full image button. 
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
 
-# Find the relative image url.
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # Parse the resulting html with soup.
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling.
+    try:
+
+        # Find the relative image url.
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL. 
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    return img_url
 
 
-# Use the base URL to create an absolute URL. 
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+def mars_facts():
+    # Add try/except for error handling.
+    try:
+        # Use 'read_html' to scrape the facts table into a DataFrame.
+        df = pd.read_html('https://galaxyfacts-mars.com')[0] #using [0] pulls the first table/item in the list.
+
+    except BaseException:
+        return None
+
+    # Assign columns and set index of DataFrame.
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
 
-# Scrape the entire table of Mars facts.
-df = pd.read_html('https://galaxyfacts-mars.com')[0] #using [0] pulls the first table/item in the list.
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
 
+    # Convert the DataFrame back into HTML code, add bootstrap.
+    return df.to_html(classes="table table-striped")
 
-# Convert the DataFrame back into HTML code.
-df.to_html()
-
-
-# End session/call.
-browser.quit
+# Final step.
+if __name__ == "__main__":
+    
+    # If running as script, print scraped data.
+    print(scrape_all())
 
